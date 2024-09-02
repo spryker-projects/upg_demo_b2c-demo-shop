c4796381aea328104c955f3f2f51c2ccb6b262d9
upgrader <upgrader@spryker.com>
2024-09-02 14:11:26 +0000

    Applied release group `CC-26451 Fixed Insecure Password Reset Workflow`, RG link https://api.release.spryker.com/release-group/5137

--- src/Pyz/Zed/Product/ProductConfig.php
+++ src/Pyz/Zed/Product/ProductConfig.php
@@ -13,7 +13,9 @@ use Spryker\Zed\Product\Dependency\ProductEvents;
 use Spryker\Zed\Product\ProductConfig as SprykerProductConfig;
 use Spryker\Zed\ProductCategory\Dependency\ProductCategoryEvents;
 use Spryker\Zed\ProductImage\Dependency\ProductImageEvents;
+use Spryker\Zed\ProductLabel\Dependency\ProductLabelEvents;
 use Spryker\Zed\ProductReview\Dependency\ProductReviewEvents;
+use Spryker\Zed\ProductSearch\Dependency\ProductSearchEvents;
 
 class ProductConfig extends SprykerProductConfig
 {
@@ -30,6 +32,16 @@ class ProductConfig extends SprykerProductConfig
             ProductImageEvents::PRODUCT_IMAGE_PRODUCT_ABSTRACT_PUBLISH,
             PriceProductEvents::PRICE_ABSTRACT_PUBLISH,
             ProductReviewEvents::PRODUCT_ABSTRACT_REVIEW_PUBLISH,
+            ProductCategoryEvents::ENTITY_SPY_PRODUCT_CATEGORY_CREATE,
+            ProductCategoryEvents::ENTITY_SPY_PRODUCT_CATEGORY_DELETE,
+            ProductLabelEvents::ENTITY_SPY_PRODUCT_LABEL_PRODUCT_ABSTRACT_CREATE,
+            ProductLabelEvents::ENTITY_SPY_PRODUCT_LABEL_PRODUCT_ABSTRACT_DELETE,
+            PriceProductEvents::ENTITY_SPY_PRICE_PRODUCT_CREATE,
+            PriceProductEvents::ENTITY_SPY_PRICE_PRODUCT_UPDATE,
+            ProductReviewEvents::ENTITY_SPY_PRODUCT_REVIEW_CREATE,
+            ProductReviewEvents::ENTITY_SPY_PRODUCT_REVIEW_UPDATE,
+            ProductImageEvents::ENTITY_SPY_PRODUCT_IMAGE_SET_CREATE,
+            ProductImageEvents::ENTITY_SPY_PRODUCT_IMAGE_SET_UPDATE,
         ];
     }
 
@@ -46,6 +58,30 @@ class ProductConfig extends SprykerProductConfig
             ProductEvents::PRODUCT_CONCRETE_PUBLISH,
             ProductBundleStorageConfig::PRODUCT_BUNDLE_PUBLISH,
             ProductImageEvents::PRODUCT_IMAGE_PRODUCT_CONCRETE_PUBLISH,
+            ProductBundleStorageConfig::ENTITY_SPY_PRODUCT_BUNDLE_CREATE,
+            ProductBundleStorageConfig::ENTITY_SPY_PRODUCT_BUNDLE_UPDATE,
+            ProductImageEvents::ENTITY_SPY_PRODUCT_IMAGE_SET_CREATE,
+            ProductImageEvents::ENTITY_SPY_PRODUCT_IMAGE_SET_UPDATE,
+            ProductImageEvents::ENTITY_SPY_PRODUCT_IMAGE_SET_TO_PRODUCT_IMAGE_CREATE,
+            ProductImageEvents::ENTITY_SPY_PRODUCT_IMAGE_SET_TO_PRODUCT_IMAGE_UPDATE,
+            PriceProductEvents::PRICE_CONCRETE_PUBLISH,
+            PriceProductEvents::ENTITY_SPY_PRICE_PRODUCT_CREATE,
+            PriceProductEvents::ENTITY_SPY_PRICE_PRODUCT_UPDATE,
+            ProductSearchEvents::ENTITY_SPY_PRODUCT_SEARCH_CREATE,
+            ProductSearchEvents::ENTITY_SPY_PRODUCT_SEARCH_UPDATE,
         ];
+    }
+
+    /**
+     * Specification:
+     * - Defines if full locale name in URL is enabled.
+     *
+     * @api
+     *
+     * @return bool
+     */
+    public function isFullLocaleNamesInUrlEnabled(): bool
+    {
+        return true;
     }
 }
